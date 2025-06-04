cd09b79c8a2e79f3dc449897e90da96c6f49ad77
Cryptology <marc@cryptology.pe>
2025-06-04 06:52:37 -0500

    test: add websocket manager tests

---
+++ __mocks__/src/infrastructure/rateLimiter.ts
@@ -0,0 +1,8 @@
+export class RateLimiter {
+    isAllowed = vi.fn<(clientId: string) => boolean>().mockReturnValue(true);
+    getRequestCount = vi.fn<(clientId: string) => number>().mockReturnValue(0);
+    clear = vi.fn<() => void>();
+    destroy = vi.fn<() => void>();
+}
+export default { RateLimiter };
+export const __esModule = true;
