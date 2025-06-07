b20810c25613a0df4ae9dc5a48d68e297227076f
Cryptology <marc@cryptology.pe>
2025-06-06 19:52:30 -0500

    fix: export all detectors

--- src/indicators/index.ts
+++ src/indicators/index.ts
@@ -1,3 +1,8 @@
 // Re-export all indicators and their types
 
 export { AccumulationDetector } from "./accumulationDetector.js";
+export { AbsorptionDetector } from "./absorptionDetector.js";
+export { ExhaustionDetector } from "./exhaustionDetector.js";
+export { DistributionDetector } from "./distributionDetector.js";
+export { DeltaCVDConfirmation } from "./deltaCVDConfirmation.js";
+export { SupportResistanceDetector } from "./supportResistanceDetector.js";
