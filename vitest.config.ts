f995ce457925555c9e0bd1db7e5b6a974d8bb91e
Cryptology <marc@cryptology.hk>
2025-09-07 18:48:22 -0500

    Vitest update

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     test: {
         environment: "node", // "jsdom" for browser tests if needed
         globals: true, // describe/it/expect as globals
-        setupFiles: ["./test/vitest.setup.ts"],
+        setupFiles: ["./backend/test/vitest.setup.ts"],
         coverage: {
             reporter: ["text", "html"],
             reportsDirectory: "./coverage",
